4d7fd10e58fc19bcc01927a0056713fff9d9de64
Rubén Darío Marquinez Rincon <86062815+DarioM70@users.noreply.github.com>
2025-08-12 23:26:21 -0500

    add requirements.txt

--- app.py
+++ app.py
@@ -67,4 +67,6 @@ def index():
     return render_template('index.html', resultado=resultado, error=error)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    from os import environ
+    port = int(environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
